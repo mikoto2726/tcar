b01c1f0a054352552e6c134cab1c2120d0ac6097
1f10220137 <s1f102201379@iniad.org>
2024-04-11 17:59:12 +0900

    a

--- 01/assign1-1.c
+++ 01/assign1-1.c
@@ -4,4 +4,8 @@ int main(int argc, char const* argv[]){
     int a = 1, b = 2;
     int *p_a, *p_b;
     printf("address a=%p, address b=%p\n", &a, &b);
+    p_a = &a;
+    p_b = &b;
+    printf("a=%d, b=%d\n", *p_a, *p_b);
+    return 0;
 }
