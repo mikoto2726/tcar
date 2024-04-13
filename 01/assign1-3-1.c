fd0ec16146c50a84432cdfce3de0b717714eb1f5
1f10220137 <s1f102201379@iniad.org>
2024-04-13 23:02:37 +0900

    1-4

--- 01/assign1-3-1.c
+++ 01/assign1-3-1.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char const* argv[]){
     int a = 0x55, b = 0xaa;
-    if ((a & (0x01 << 3 )) == 0){
+    a = a | (0x01 << 3);
+    if ((a & (0x01 << 3 )) != 0){
         printf("pass\n");
     }
     else {
--- 01/assign1-3-2.c
+++ 01/assign1-3-2.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char const* argv[]){
     int a = 0x55, b = 0xaa;
-    if ((a & (0x01 << 3 )) == 0){
+    b = b & ~(0x01 << 3);
+    if ((b & (0x01 << 3 )) == 0){
         printf("pass\n");
     }
     else {
