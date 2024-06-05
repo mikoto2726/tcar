9fe7d0c5ccba7b7c66f6a6f83b30c8d2fb92d5fb
1f10220137 <s1f102201379@iniad.org>
2024-06-05 23:28:47 +0900

    06

--- 06/exercise1.c
+++ 06/exercise1.c
@@ -4,43 +4,61 @@
 #define PEDATA 0x400C0400 //PEデータレジスタのアドレス
 #define PE_CR   0x400C0404 //PE出力コントロールレジスタ
 
+ID semid;
+
 void task_a(INT stacd, void *exinf){
+    *(_UW*)(PE_CR) |= (1 << 3);
     while(1){
+        tk_wai_sem(semid, 1, TMO_FEVR);
         for(int i=0; i<10; i++){
             *(_UB*)(PEDATA) |= (1<<3); 
             tk_dly_tsk(500);
             *(_UB*)(PEDATA) &= ~(1<<3);
             tk_dly_tsk(500);
         }
+        *(_UB*)(PEDATA) &= ~(1<<3);
+        tk_sig_sem(semid, 1);
     }    
 }
 
 void task_b(INT stacd, void *exinf){
+    *(_UW*)(PE_CR) |= (1 << 3);
     while(1){
+        tk_wai_sem(semid, 1, TMO_FEVR);
         for(int i=0; i<10; i++){
             *(_UB*)(PEDATA) |= (1<<3); 
-            tk_dly_tsk(1000);
+            tk_dly_tsk(100);
             *(_UB*)(PEDATA) &= ~(1<<3);
-            tk_dly_tsk(1000);
+            tk_dly_tsk(100);
         }
+        tk_sig_sem(semid, 1);
     }    
 }
 
 void task_c(INT stacd, void *exinf){
+    *(_UW*)(PE_CR) |= (1 << 3);
     while(1){
+        tk_wai_sem(semid, 1, TMO_FEVR);
         for(int i=0; i<10; i++){
             *(_UB*)(PEDATA) |= (1<<3); 
-            tk_dly_tsk(10000);
+            tk_dly_tsk(1000);
             *(_UB*)(PEDATA) &= ~(1<<3);
-            tk_dly_tsk(10000);
+            tk_dly_tsk(1000);
         }
+        tk_sig_sem(semid, 1);
     }    
 }
 
 EXPORT int usermain(void){
     T_CTSK ctsk;
+    T_CSEM csem;
     ID tskid_a, tskid_b, tskid_c;
 
+    csem.sematr  = TA_TFIFO;
+    csem.isemcnt = 1;
+    csem.maxsem   = 1;
+    semid = tk_cre_sem(&csem);
+ 
     ctsk.tskatr  = TA_HLNG | TA_RNG3;
     ctsk.task    = (FP)task_a;
     ctsk.itskpri = 10;
@@ -48,17 +66,17 @@ EXPORT int usermain(void){
     tskid_a      = tk_cre_tsk(&ctsk);
 
     ctsk.task    = (FP)task_b;
-    ctsk.itskpri = 10;
     tskid_b      = tk_cre_tsk(&ctsk);
 
     ctsk.task    = (FP)task_c;
-    ctsk.itskpri = 10;
     tskid_c      = tk_cre_tsk(&ctsk);
 
     tk_sta_tsk(tskid_a, 0);
     tk_sta_tsk(tskid_b, 0);
     tk_sta_tsk(tskid_c, 0);
+    tk_sta_tsk(semid, 0);
     
     tk_slp_tsk(TMO_FEVR);
+    
     return 0;
 }
--- 06/exercise2.c
+++ 06/exercise2.c
@@ -4,43 +4,60 @@
 #define PEDATA 0x400C0400 //PEデータレジスタのアドレス
 #define PE_CR   0x400C0404 //PE出力コントロールレジスタ
 
+ID mtxid;
+
 void task_a(INT stacd, void *exinf){
+    *(_UW*)(PE_CR) |= (1 << 3);
     while(1){
+        tk_loc_mtx(mtxid, 1, TMO_FEVR);
         for(int i=0; i<10; i++){
             *(_UB*)(PEDATA) |= (1<<3); 
             tk_dly_tsk(500);
             *(_UB*)(PEDATA) &= ~(1<<3);
             tk_dly_tsk(500);
         }
+        tk_unl_mtx(mtxid);
     }    
 }
 
 void task_b(INT stacd, void *exinf){
+    *(_UW*)(PE_CR) |= (1 << 3);
     while(1){
+        tk_loc_mtx(mtxid, 1, TMO_FEVR);
         for(int i=0; i<10; i++){
             *(_UB*)(PEDATA) |= (1<<3); 
-            tk_dly_tsk(1000);
+            tk_dly_tsk(100);
             *(_UB*)(PEDATA) &= ~(1<<3);
-            tk_dly_tsk(1000);
+            tk_dly_tsk(100);
         }
+        tk_unl_mtx(mtxid);
     }    
 }
 
 void task_c(INT stacd, void *exinf){
+    *(_UW*)(PE_CR) |= (1 << 3);
     while(1){
+        tk_loc_mtx(mtxid, 1, TMO_FEVR);
         for(int i=0; i<10; i++){
             *(_UB*)(PEDATA) |= (1<<3); 
-            tk_dly_tsk(10000);
+            tk_dly_tsk(1000);
             *(_UB*)(PEDATA) &= ~(1<<3);
-            tk_dly_tsk(10000);
+            tk_dly_tsk(1000);
         }
+        tk_unl_mtx(mtxid);
     }    
 }
 
 EXPORT int usermain(void){
     T_CTSK ctsk;
+    T_CSEM cmtx;
     ID tskid_a, tskid_b, tskid_c;
 
+    cmtx.sematr  = TA_TFIFO;
+    cmtx.isemcnt = 1;
+    cmtx.maxsem   = 1;
+    mtxid = tk_cre_mtx(&cmtx);
+ 
     ctsk.tskatr  = TA_HLNG | TA_RNG3;
     ctsk.task    = (FP)task_a;
     ctsk.itskpri = 10;
@@ -48,17 +65,17 @@ EXPORT int usermain(void){
     tskid_a      = tk_cre_tsk(&ctsk);
 
     ctsk.task    = (FP)task_b;
-    ctsk.itskpri = 10;
     tskid_b      = tk_cre_tsk(&ctsk);
 
     ctsk.task    = (FP)task_c;
-    ctsk.itskpri = 10;
     tskid_c      = tk_cre_tsk(&ctsk);
 
     tk_sta_tsk(tskid_a, 0);
     tk_sta_tsk(tskid_b, 0);
     tk_sta_tsk(tskid_c, 0);
+    tk_sta_tsk(semid, 0);
     
     tk_slp_tsk(TMO_FEVR);
+    
     return 0;
 }
