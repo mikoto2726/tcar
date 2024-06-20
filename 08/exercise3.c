f9944a9d4c0444ac33f97b1e7efc7da9a84cb620
1f10220137 <s1f102201379@iniad.org>
2024-06-20 19:14:17 +0900

    課題3成功

--- 08/exercise3.c
+++ 08/exercise3.c
@@ -12,48 +12,63 @@
 
 #define PEDATA 0x400C0400 // PEデータレジスタのアドレス
 #define PECR   0x400C0404 // PE出力コントロールレジスタ
-    
 
+ID led_tskid; // LED制御タスクID
 
-void led_tskid(INT stack, void *exinf) {
-    *(_UW*)(PECR) |= (1<<3); //PE3出力許可
+void led_task(INT stack, void *exinf) {
+    *(_UW*)(PECR) |= (1<<3); // PE3出力許可
 
-    while(1){
-        *(_UW*)(PEDATA) |= (1<<3); //PE3'High'出力
+    while (1) {
+        tk_slp_tsk(TMO_FEVR);
+        *(_UW*)(PEDATA) |= (1<<3); // PE3'High'出力
         tk_dly_tsk(500); // 500 ミリ秒待つ
-        *(_UW*)(PEDATA) &= ~(1<<3); //PE3'Low'出力
+        *(_UW*)(PEDATA) &= ~(1<<3); // PE3'Low'出力
         tk_dly_tsk(500); // 500 ミリ秒待つ
-    } 
+    }
 }
 
-void inthdr(UINT intno){
+void inthdr(UINT intno) {
     ClearInt(INT3);
     tk_wup_tsk(led_tskid);
     return;
 }
 
 EXPORT int usermain(void) {
+    T_CTSK ctsk; // タスク生成情報
+    T_DINT dint; // 割り込みハンドラ定義情報
     UINT intsts;
-    
-    DI(intsts);//全ての割り込みを禁止
 
-    T_DINT dint; //割り込みハンドラ定義情報
-    //割り込みハンドラ定義
-    dint.intatr = TA_HLNG; //割り込みハンドラ属性
-    dint.inthdr = inthdr; //割り込みハンドラの実行アドレス(割り込みハンドラの関数名)
-    tk_def_int(INT3, &dint); //割り込みハンドラの定義
+    // LED制御タスクの生成
+    ctsk.tskatr = TA_HLNG | TA_RNG3;
+    ctsk.exinf = 0;
+    ctsk.task = (FP)led_task;
+    ctsk.itskpri = 10;
+    ctsk.stksz = 1024;
+    led_tskid = tk_cre_tsk(&ctsk);
+    tk_sta_tsk(led_tskid, 0);
 
-    *(_UW*)PACR &= ~(1 << 3);  //PA3ポート端子の出力禁止
-    *(_UW*)PAIE |= (1 << 3);   //PA3ポート端子の入力許可
-    *(_UW*)PAFR3 &= ~(1 << 3); //PA3ポート端子をINT3に選択
+    DI(intsts); // 全ての割り込みを禁止
+
+    // 割り込みハンドラ定義
+    dint.intatr = TA_HLNG; // 割り込みハンドラ属性
+    dint.inthdr = inthdr; // 割り込みハンドラの実行アドレス
+    tk_def_int(INT3, &dint); // 割り込みハンドラの定義
+
+    // GPIO設定
+    *(_UW*)PACR &= ~(1 << 3); // PA3を入力に設定
+    *(_UW*)PAIE |= (1 << 3); // PA3の割り込み許可
+    *(_UW*)PAFR3 &= ~(1 << 3); // PA3を通常のGPIOとして設定
     SetIntMode(INT3, IM_EDGE | IM_LOW); // 割り込み信号を立下りで検出に設定
 
-    ClearInt(INT3); //割り込み要因クリア
-    EnableInt(INT3, 5); //割り込み有効&優先度設定(優先度は0~7)
+    ClearInt(INT3); // 割り込み要因クリア
+    EnableInt(INT3, 5); // 割り込み有効&優先度設定(優先度は0~7)
 
-    EI(intsts); //DIによる割り込み禁止の解除
+    EI(intsts); // DIによる割り込み禁止の解除
 
-    
+    while (1) {
+        tk_slp_tsk(TMO_FEVR); // 無限待機
+    }
 
     return 0;
 }
+                                                                    
